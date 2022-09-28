8918affa652d6c33fce8b7e9d855f85defd403ec
satriopriambodo <satriopriambodo96@gmail.com>
2022-09-28 14:47:24 +0700

    first

---
+++ pages/news/index.js
@@ -0,0 +1,20 @@
+import { Fragment } from "react";
+import Link from "next/link";
+
+function NewsPage() {
+  return (
+    <Fragment>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/nextjs-is-a-great-framework">
+            NextJS is A Great Framework
+          </Link>
+        </li>
+        <li>Something Else</li>
+      </ul>
+    </Fragment>
+  );
+}
+
+export default NewsPage;
